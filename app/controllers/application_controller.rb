0ea390176f6a6910042123e2ab66ee222133faca
Thomas Wesley Matlock <thomasmatlock1994@gmail.com>
2018-09-01 16:05:22 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -20,7 +20,7 @@ class ApplicationController < Sinatra::Base
 end
 
   get '/account' do
-    @current_user = User.find_b
+    @current_user = User.find_by_id()
     if @current_user
       erb :account
     else
