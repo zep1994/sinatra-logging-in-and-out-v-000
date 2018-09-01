9fdaf9adf22f2a6e841ae5d225b31cec946ba966
Thomas Wesley Matlock <thomasmatlock1994@gmail.com>
2018-09-01 15:59:41 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -21,11 +21,7 @@ class ApplicationController < Sinatra::Base
 end
 
   get '/account' do
-    if Helpers.is_logged_in?(session)
       erb :account
-    else 
-      erb :error
-    end
   end
 
   get '/logout' do
